fb459c452e85a1161b231127f266aa8ef70fde39
xLima12 <felipelima1910@gmail.com>
2025-04-03 13:44:52 -0300

    feat add class Author and function add new author

---
+++ src/br/com/codenoir/Author.java
@@ -0,0 +1,39 @@
+package br.com.codenoir;
+
+import java.time.LocalDate;
+
+public class Author {
+    private int id;
+    private String name;
+    private LocalDate birth;
+
+    public Author(int id, String name, LocalDate birth) {
+        this.id = id;
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+}
