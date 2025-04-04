a3274b8e6a0caf3fe7a99f87b9d75c0ecb6bb848
xLima12 <felipelima1910@gmail.com>
2025-04-04 11:54:47 -0300

    feat: add new functions to get all users and get all authors

--- src/Main.java
+++ src/Main.java
@@ -4,70 +4,171 @@ import br.com.codenoir.Client;
 import br.com.codenoir.Library;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean exec = true;
-        String option;
-        String bookId;
-        String clientName;
         Scanner scan = new Scanner(System.in);
+        int option = -1;
+        String userName;
+        String userBirth;
+        String userEmail;
+        String authorName;
+        String authorBirth;
+        String bookTitle;
+        int bookId;
 
-        Library library = new Library();
+//        Author author1 = new Author(1, "José Saramago", LocalDate.of(1922, 11, 16));
+//        Author author2 = new Author(2, "Clarice Lispector", LocalDate.of(1920, 12, 10));
+//        Author author3 = new Author(3, "Edgar Allan Poe", LocalDate.of(1809, 1, 19));
+//        Author author4 = new Author(4, "Fiódor Dostoiévski", LocalDate.of(1821, 11, 11));
+//        Author author5 = new Author(5, "William Shakespeare", LocalDate.of(1564, 4, 23));
+//        Author author6 = new Author(6, "Marcel Proust", LocalDate.of(1871, 7, 10));
 
-        Client client1 = new Client(1, "Felipe", LocalDate.of(1996, 2, 20), "felipe@gmail.com");
-        Client client2 = new Client(2, "Julia", LocalDate.of(2020, 6, 24), "felipe@gmail.com");
-        library.addClient(client1);
-        library.addClient(client2);
+//        Book book1 = new Book(1, "Memorial do convento", library.getAuthor(1));
+//        Book book2 = new Book(2, "Laços de família", library.getAuthor(2));
+//        Book book3 = new Book(3, "O poço e o pêndulo", library.getAuthor(3));
+//        Book book4 = new Book(4, "Crime e Castigo", library.getAuthor(4));
+//        Book book5 = new Book(5, "Hamlet", library.getAuthor(5));
+//        Book book6 = new Book(6, "Em busca do tempo perdido", library.getAuthor(6));
 
-        Author author1 = new Author(1, "José Saramago", LocalDate.of(1922, 11, 16));
-        Author author2 = new Author(2, "Clarice Lispector", LocalDate.of(1920, 12, 10));
-        Author author3 = new Author(3, "Edgar Allan Poe", LocalDate.of(1809, 1, 19));
-        Author author4 = new Author(4, "Fiódor Dostoiévski", LocalDate.of(1821, 11, 11));
-        Author author5 = new Author(5, "William Shakespeare", LocalDate.of(1564, 4, 23));
-        Author author6 = new Author(6, "Marcel Proust", LocalDate.of(1871, 7, 10));
-        library.addAuthor(author1);
-        library.addAuthor(author2);
-        library.addAuthor(author3);
-        library.addAuthor(author4);
-        library.addAuthor(author5);
-        library.addAuthor(author6);
+        header();
+        while (option != 0) {
+            menu();
 
-        Book book1 = new Book(1, "Memorial do convento", library.getAuthor(1));
-        Book book2 = new Book(2, "Laços de família", library.getAuthor(2));
-        Book book3 = new Book(3, "O poço e o pêndulo", library.getAuthor(3));
-        Book book4 = new Book(4, "Crime e Castigo", library.getAuthor(4));
-        Book book5 = new Book(5, "Hamlet", library.getAuthor(5));
-        Book book6 = new Book(6, "Em busca do tempo perdido", library.getAuthor(6));
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-        library.addBook(book4);
-        library.addBook(book5);
-        library.addBook(book6);
-
-        System.out.println("Deseja verificar os livros disponíveis?");
-        while (exec) {
-            option = scan.nextLine().toLowerCase();
+            option = scan.nextInt();
             switch (option) {
-                case "sim":
-                    library.getAvailableBooks();
-                    System.out.println("Informe o ID do livro que deseja");
-                    bookId = scan.nextLine();
-                    System.out.println("Informe o seu nome?");
-                    clientName = scan.nextLine();
-                    library.getLoanBook(Integer.parseInt(bookId), clientName);
+                case 1:
+                    scan.nextLine();
+                    System.out.println("Informe o seu nome: ");
+                    userName = scan.nextLine();
+                    System.out.println("Informe o seu nascimento (dd/MM/aaaa): ");
+                    userBirth = scan.nextLine();
+                    System.out.println("Informe o seu e-mail: ");
+                    userEmail = scan.nextLine();
+                    registerUser(userName, userBirth, userEmail);
                     break;
-                case "nao":
-                    System.out.println("Encerrando o programa. Obrigado por usar.");
-                    exec = false;
+                case 2:
+                    scan.nextLine();
+                    System.out.println("Informe o nome do autor: ");
+                    authorName = scan.nextLine();
+                    System.out.println("Informe o nascimento do autor: ");
+                    authorBirth = scan.nextLine();
+                    registerAuthor(authorName, authorBirth);
                     break;
-                case "emprestados":
-                    library.getListLoanBook();
-                default:
-                    System.out.println("Opcao inválida! Por favor digite Sim ou Nao");
+                case 3:
+                    scan.nextLine();
+                    System.out.println("Informe o título do livro: ");
+                    bookTitle = scan.nextLine();
+                    System.out.println("Informe o nome do autor: ");
+                    authorName = scan.nextLine();
+                    registerBook(bookTitle, authorName);
+                    break;
+                case 4:
+                    getAvailableBook();
+                    break;
+                case 5:
+                    getLoanBook();
+                    break;
+                case 6:
+                    if(getListBooks()) {
+                        System.out.println("Informe o id do livro que deseja pegar emprestado: ");
+                        bookId = scan.nextInt();
+                        scan.nextLine();
+                        System.out.println("Informe o seu nome cadastrado: ");
+                        userName = scan.nextLine();
+                        registerLoanBook(bookId, userName);
+                    }
+                    System.out.println("Nenhum livro cadastro para emprestimo!");
+                    break;
+                case 7:
+                    getListClients();
+                    break;
+                case 8:
+                    getListAuthors();
+                    break;
+                case 0:
+                    System.out.println("Obrigado por usar o sistema.");
+                    break;
             }
         }
+    }
+
+    public static void header() {
+        System.out.println("----------------------");
+        System.out.println("| Library Management |");
+        System.out.println("----------------------");
+        System.out.println("Bem vindo ao sistema");
+    }
+
+    public static void menu() {
+        System.out.println("----------------------");
+        System.out.println("1 - Cadastrar usuário");
+        System.out.println("2 - Cadastrar Autor");
+        System.out.println("3 - Cadastrar livro");
+        System.out.println("4 - Consultar livros disponíveis");
+        System.out.println("5 - Consultar livros emprestados");
+        System.out.println("6 - Solicitar empréstimo de livro(s)");
+        System.out.println("7 - Consultar usuarios");
+        System.out.println("8 - Consultar autores");
+        System.out.println("0 - Sair");
+        System.out.println("----------------------");
+    }
+
+    public static void registerUser(String name, String birth, String email) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(birth, formatter);
+        Client client = new Client(name, date, email);
+        useLibrary().addClient(client);
+    }
+
+    public static void registerAuthor(String name, String birth) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(birth, formatter);
+        Author author = new Author(name, date);
+        useLibrary().addAuthor(author);
+    }
+
+    public static void registerBook(String title, String name) {
+        Author author = useLibrary().findAuthorByName(name);
+
+        if(author == null) {
+            System.out.println("Autor não encontrado!");
+            return;
+        }
+
+        Book book = new Book(title, author);
+        useLibrary().addBook(book);
+        System.out.println("Livro registrado com sucesso!");
+    }
+
+    public static void registerLoanBook(int id, String name) {
+        useLibrary().getLoanBook(id, name);
+    }
+
+    public static void getAvailableBook() {
+        useLibrary().getAvailableBooks();
+    }
+
+    public static void getLoanBook() {
+        useLibrary().getListLoanBook();
+    }
+
+    public static void getListAuthors() {
+        System.out.println(useLibrary().getListAuthor());
+    }
+
+    public static boolean getListBooks() {
+        return useLibrary().getListBook();
+    }
+
+    public static void getListClients() {
+        useLibrary().getListClients();
+    }
+
+    public static Library useLibrary() {
+        return Library.getInstance();
     }
 }
--- src/br/com/codenoir/Library.java
+++ src/br/com/codenoir/Library.java
@@ -5,10 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
+    private static Library instance;
     private List<Book> books = new ArrayList<>();
     private List<Author> authors = new ArrayList<>();
     private List<LoanBook> loanBooks = new ArrayList<>();
     private List<Client> clients = new ArrayList<>();
+
+    private Library() {}
+
+    public static Library getInstance() {
+        if(instance == null) {
+            instance = new Library();
+        }
+        return instance;
+    }
 
     public void addBook(Book book) {
         this.books.add(book);
@@ -27,45 +37,86 @@ public class Library {
     }
 
     public void getLoanBook(int id, String name) {
-        if(!this.books.get(id -1).isAvailable()){
+        int idBook = id - 1;
+
+        if(!this.books.get(idBook).isAvailable()){
             System.out.println("O livro informado não está disponível.");
         }
 
-        if(this.clients.contains(name)) {
-            System.out.println("Necessário realizar o cadastro no sistema para prosseguir com o emprestimo do livro escolhido.");
+        if(this.findClientByName(name) == null) {
+            System.out.println("Necessário realizar o cadastro no sistema para prosseguir com o empréstimo do livro escolhido.");
         }
 
-        this.books.get(id -1).setAvailable(false);
+        this.books.get(idBook).setAvailable(false);
 
-        int client = this.clients.indexOf(name);
+        int idClient = this.findClientByName(name).getId();
 
-        LoanBook loanBook = new LoanBook(this.books.get(id -1), this.clients.get(client + 1), LocalDate.now(), LocalDate.now().plusDays(3));
+        LoanBook loanBook = new LoanBook(this.books.get(idBook), this.clients.get(idClient), LocalDate.now(), LocalDate.now().plusMonths(3));
         this.addLoanBook(loanBook);
-        System.out.println("Livro emprestado com sucesso.");
+        System.out.println("Livro emprestado com sucesso!");
     }
 
     public void getListLoanBook() {
+        if(this.loanBooks.isEmpty()) {
+            System.out.println("Nenhum livro emprestado no momento.");
+        }
+
         for(LoanBook loanBook : loanBooks) {
             System.out.println(String.format("Livro %s emprestado para %s em %td/%<tm/%<ty até %tD ", loanBook.getBook().getTitle(), loanBook.getClient().getName(), loanBook.getLoanAt(), loanBook.getReturnAt()));
         }
     }
 
-    public Author getAuthor(int id) {
-        return this.authors.get(id -1);
+    public List<Author> getListAuthor() {
+        return authors;
+    }
+
+    public Client findClientByName(String name) {
+        for(Client client : clients) {
+            if(client.getName().equalsIgnoreCase(name)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    public Author findAuthorByName(String name) {
+        for(Author author : authors) {
+            if(author.getName().equalsIgnoreCase(name)) {
+                return author;
+            }
+        }
+        return null;
     }
 
     public void getAvailableBooks() {
         List<Book> booksAvailable = new ArrayList<>();
-        for(Book book: books) {
-            if(book.isAvailable()) {
-                booksAvailable.add(book);
-                displayInfo(book.getId(), book.getTitle(), book.getAuthor().getName());
+
+        if(this.books.isEmpty()){
+            System.out.println("Nenhum livro disponível no momento.");
+        } else {
+            for(Book book: books) {
+                if(book.isAvailable()) {
+                    booksAvailable.add(book);
+                    System.out.println(String.format("ID: %d | Titulo: %-30.30s | Autor: %s",
+                            book.getId(), book.getTitle(), book.getAuthor()));
+                }
             }
         }
     }
 
-    public void displayInfo(int id, String title, String author) {
-        String result = String.format("ID: %d | Titulo: %-30.30s | Autor: %s", id, title, author);
-        System.out.println(result);
+    public boolean getListBook() {
+        return !this.books.isEmpty();
     }
+
+    public void getListClients() {
+        if(this.clients.isEmpty()) {
+            System.out.println("Nenhum cliente cadastrado no momento.");
+        } else {
+            for(Client client : clients) {
+                System.out.println(String.format("Id: %d | Nome: %-15.15s | Nascimento: %tD | E-mail: %s",
+                        client.getId(), client.getName(), client.getBirth(), client.getEmail()));
+            }
+        }
+    }
+
 }
